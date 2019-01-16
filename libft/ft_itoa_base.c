d3defd386315b2f17a12e23d93974410aa8d192b
Alexis TYCZYNSKI <atyczyns@e1r10p16.42.fr>
2019-01-16 13:46:45 +0100

    itoa_base

--- libft/ft_itoa_base.c
+++ libft/ft_itoa_base.c
@@ -6,62 +6,37 @@
 /*   By: atyczyns <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/11 09:29:17 by atyczyns          #+#    #+#             */
-/*   Updated: 2018/12/11 10:57:42 by atyczyns         ###   ########.fr       */
+/*   Updated: 2019/01/16 13:46:22 by atyczyns         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-static char		*ft_result(int value, int base, int i, int neg)
-{
-	int		tmp;
-	int		k;
-	char	*result;
-
-	result = (char *)malloc(sizeof(char) * (i + 1));
-	result[i + 1] = '\0';
-	while (value >= base)
-	{
-		tmp = value % base;
-		if (tmp <= 9)
-			k = '0';
-		else
-			tmp = 'A' - 10;
-		result[i] = tmp + k;
-		--i;
-		value = value / base;
-	}
-	if (neg == -1)
-		result[0] = '-';
-	if (value <= 9)
-		k = '0';
-	else
-		k = 'A' - 10;
-	result[i] = value + k;
-	return (result);
-}
-
 char			*ft_itoa_base(int value, int base)
 {
 	int		i;
-	int		cpy;
 	int		neg;
+	long	value2;
+	char	*result;
+	int		tmp;
 
-	neg = 1;
-	i = 1;
-	if (value < 0 && base != 10)
-		value = -value;
-	else if (value < 0)
-	{
-		value = -value;
-		neg = -1;
+	value2 = value;
+	value2 = (value2 < 0) ? -(value2 * base) : (value2 * base);
+	neg = (value < 0 && base == 10) ? -1 : 1;
+	i = (value < 0 && base == 10) ? 2 : 1;
+	while ((value2 /= base) >= base * base)
 		++i;
-	}
-	cpy = value;
-	while (cpy / base > base - 1)
+	value2 = value;
+	value2 = (value2 < 0) ? -(value2 * base) : (value2 * base);
+	if (!(result = (char *)malloc(sizeof(char) * (i + 1))))
+		return (NULL);
+	result[i + 1] = '\0';
+	while (value2 >= base)
 	{
-		++i;
-		cpy = cpy / base;
+		tmp = (value2 /= base) % base;
+		result[i--] = (tmp <= 9) ? (tmp + '0') : (tmp + 'A' - 10);
 	}
-	return (ft_result(value, base, i, neg));
+	result[i] = (value2 <= 9) ? (value2 + '0') : (value2 + 'A' - 10);
+	result[0] = (neg == -1) ? '-' : result[0];
+	return (result);
 }
